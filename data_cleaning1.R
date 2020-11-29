96a741bcc95db7bf76dcfe06033ae72c91b7d99e
Valerie Lindstrom <vlindstrom@outlook.com>
2020-11-29 16:38:24 -0700

    data cleaning for top 3 colleges for timeseries

--- data_cleaning1.R
+++ data_cleaning1.R
@@ -3,11 +3,15 @@
 library(tidyverse)
 library(dplyr)
 library(ggplot2)
+library(readxl)
+library(lubridate)
 
 college_raw <- 
   read_csv(
     "https://raw.githubusercontent.com/nytimes/covid-19-data/master/colleges/colleges.csv") 
 
+
+#top 5 colleges by state with highest COVID cases
 top_5 <- college_raw %>%
   select(state, county, city, college, cases) %>%
   group_by(state) %>%
@@ -22,4 +26,58 @@ top_state <- college_raw %>%
 
 ggplot(data = top_state, mapping = aes(x = cases, y = state)) +
   geom_point(mapping = aes(x = cases))
+
+#top 3 schools with highest cases for time series data, include reference point
+#of when school started
+
+top_3 <- college_raw %>%
+  select(state, county, city, college, cases) %>%
+  group_by(state) %>%
+  arrange(desc(cases)) %>%
+  head(3)
+
+# add and clean clemson's covid data
+clemson <- read_excel("clemsonDashboard.xlsx", sheet = "Daily Data") 
+
+clemson_cases <- clemson %>%
+  select(Date, Positive) 
+
+# add and clean OSU covid data
+osu <- read_excel("Ohio State COVID-19 Dashboard Data Tables.xlsx", skip = 5)
+
+osu_cases <- osu %>%
+  select(`Test Date`, `Positive Tests...9`) %>%
+  rename(Data = `Test Date`, 
+         Positive = `Positive Tests...9`)
+
+
+all_colleges %>%
+  #join datasets
+
+top_3_timeseries <- top_3 %>%
+  #add in data from each college's covid tracking to track # of cases by week
+
+#clemson start date: Aug 19
+#UF: Aug 31
+#OSU: Aug 25
+
+#top school with COVID for density map 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
